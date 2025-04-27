6a48e8502b59cafc1887fa5e63e0e01a93553043
Theodoros Kyriakidis <thekyria@gmail.com>
2025-04-27 15:30:46 +0300

    moving markers to mongodb

--- api/markers.js
+++ api/markers.js
@@ -1,9 +1,31 @@
 
-export default async function handler(req, res) {
-    const markers = [
-        { title: "Holy Sepulchre", lat: 31.77846303313139, lng: 35.22971821508876 },
-        { title: "Louvre Museum", lat: 48.8606, lng: 2.3376 }
-    ];
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri = process.env.MONGODB_URI;
 
-    res.status(200).json(markers);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+export default async function handler(req, res) {
+    try {
+        await client.connect();
+        const db = client.db("hagioi-maps");
+        const collection = db.collection("markers");
+
+        const markers = await collection.find({}).toArray();
+        res.status(200).json(markers);
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        res.status(500).json({ message: "Error fetching markers from MongoDB" });
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
